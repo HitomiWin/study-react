04a53ef403a853715187a345cfd074439ae26fda
HitomiWin <hitomi.winberg@gmail.com>
2023-09-14 15:06:13 +0200

    fixed prettier and lint

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,42 @@
+"use client";
+
+import React, { useCallback, useState, useEffect } from "react";
 import styles from "./page.module.css";
 import Head from "next/head";
 import { Header } from "./components";
 
-export default function Home() {
+const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const getPosts = useCallback(async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const json = await res.json();
+    setPosts(json);
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      <main>Index</main>
+      <main>
+        {posts.length > 0 ? (
+          <ol>
+            {posts.map((post) => (
+              <div key={post.id}>
+                <li>{post.title}</li>
+              </div>
+            ))}
+          </ol>
+        ) : null}
+      </main>
     </div>
   );
-}
+};
+
+export default Home;
