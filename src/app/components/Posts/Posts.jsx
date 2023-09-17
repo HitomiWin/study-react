63b172aeedfdd744b3048799e373856b84fb1483
HitomiWin <hitomi.winberg@gmail.com>
2023-09-17 15:37:42 +0200

    reproduce with useReducer

--- src/app/components/Posts/Posts.jsx
+++ src/app/components/Posts/Posts.jsx
@@ -1,11 +1,33 @@
 "use client";
 
-import React, { useCallback, useState, useEffect } from "react";
+import React, { useCallback, useEffect, useReducer } from "react";
+const initialState = {
+  data: [],
+  loading: true,
+  error: null,
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "end":
+      return {
+        ...state,
+        data: action.data,
+        loading: false,
+      };
+    case "error":
+      return {
+        ...state,
+        error: action.error,
+        loading: false,
+      };
 
+    default: {
+      throw new Error("no such action type");
+    }
+  }
+};
 export const Posts = () => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, initialState);
   const getPosts = useCallback(async () => {
     try {
       const res = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -13,31 +35,29 @@ export const Posts = () => {
         throw new Error("Something went wrong.");
       }
       const json = await res.json();
-      setPosts(json);
+      dispatch({ type: "end", data: json });
     } catch (error) {
-      setError(error);
+      dispatch({ type: "error", error });
     }
-
-    setLoading(false);
   }, []);
 
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  if (loading) {
+  if (state.loading) {
     return <div>...Loading</div>;
   }
-  if (error) {
-    return <div>{error.message}</div>;
+  if (state.error) {
+    return <div>{state.error.message}</div>;
   }
-  if (posts.length === 0) {
+  if (state.data.length === 0) {
     return <div>No Data</div>;
   }
 
   return (
     <ol>
-      {posts.map((post) => (
+      {state.data.map((post) => (
         <div key={post.id}>
           <li>{post.title}</li>
         </div>
