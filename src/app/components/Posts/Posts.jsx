2cf28872b71b1ef85ee27e9c18e09e2e09a8b3e7
HitomiWin <hitomi.winberg@gmail.com>
2023-09-19 11:42:43 +0200

    Replaced to swr

--- src/app/components/Posts/Posts.jsx
+++ src/app/components/Posts/Posts.jsx
@@ -1,63 +1,22 @@
 "use client";
+import { usePosts } from "@/app/hooks/usePosts";
 
-import React, { useCallback, useEffect, useReducer } from "react";
-const initialState = {
-  data: [],
-  loading: true,
-  error: null,
-};
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "end":
-      return {
-        ...state,
-        data: action.data,
-        loading: false,
-      };
-    case "error":
-      return {
-        ...state,
-        error: action.error,
-        loading: false,
-      };
-
-    default: {
-      throw new Error("no such action type");
-    }
-  }
-};
 export const Posts = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const getPosts = useCallback(async () => {
-    try {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      if (!res.ok) {
-        throw new Error("Something went wrong.");
-      }
-      const json = await res.json();
-      dispatch({ type: "end", data: json });
-    } catch (error) {
-      dispatch({ type: "error", error });
-    }
-  }, []);
+  const { data, error, isLoading, isEmpty } = usePosts();
 
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-
-  if (state.loading) {
+  if (isLoading) {
     return <div>...Loading</div>;
   }
-  if (state.error) {
-    return <div>{state.error.message}</div>;
+  if (error) {
+    return <div>{error.message}</div>;
   }
-  if (state.data.length === 0) {
+  if (isEmpty) {
     return <div>No Data</div>;
   }
 
   return (
     <ol>
-      {state.data.map((post) => (
+      {data.map((post) => (
         <div key={post.id}>
           <li>{post.title}</li>
         </div>
